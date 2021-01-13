f019efc49eb7424a0b50b6ae4f69eee5ec1cd18d
Katerina Lina Papaloukas <katpapaloukas@gmail.com>
2021-01-13 12:39:28 -0500

    added validation items to some of the model keys

--- server/db/models/cartItems.js
+++ server/db/models/cartItems.js
@@ -3,27 +3,33 @@ const db = require('../db')
 
 const CartItems = db.define('cartItems', {
   id: {
-    type: Sequelize.STRING,
+    type: Sequelize.INTEGER,
     unique: true,
     allowNull: false
   },
   //is this supposed to be product name?
   //what is the difference with productID and cartID and ID?
   productId: {
-    type: Sequelize.STRING
+    type: Sequelize.INTEGER,
+    allowNull: false
   },
   cartId: {
-    type: Sequelize.STRING
+    type: Sequelize.INTEGER,
+    allowNull: false
   },
   price: {
-    type: Sequelize.DECIMAL
+    type: Sequelize.FLOAT,
+    allowNull: false,
+    default: 0
   },
   //implemented a min of 0 for inventory (no negative items)
   quantity: {
     type: Sequelize.INTEGER,
+    allowNull: false,
     validate: {
       min: 0
-    }
+    },
+    default: 0
   },
   //is this the name/info of the product?
   content: {
