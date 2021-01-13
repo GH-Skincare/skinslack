9991138f45d2097307c8e9d96eaa944f1ccd93c1
Katerina Lina Papaloukas <katpapaloukas@gmail.com>
2021-01-13 12:50:11 -0500

    updated cartItems model

--- server/db/models/cartItems.js
+++ server/db/models/cartItems.js
@@ -2,26 +2,19 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const CartItems = db.define('cartItems', {
-  id: {
-    type: Sequelize.INTEGER,
-    unique: true,
-    allowNull: false
-  },
-  //is this supposed to be product name?
-  //what is the difference with productID and cartID and ID?
-  productId: {
-    type: Sequelize.INTEGER,
-    allowNull: false
-  },
-  cartId: {
-    type: Sequelize.INTEGER,
-    allowNull: false
-  },
-  price: {
-    type: Sequelize.FLOAT,
-    allowNull: false,
-    default: 0
-  },
+  // productId: {
+  //   type: Sequelize.INTEGER,
+  //   allowNull: false
+  // },
+  // cartId: {
+  //   type: Sequelize.INTEGER,
+  //   allowNull: false
+  // },
+  // price: {
+  //   type: Sequelize.FLOAT,
+  //   allowNull: false,
+  //   default: 0
+  // },
   //implemented a min of 0 for inventory (no negative items)
   quantity: {
     type: Sequelize.INTEGER,
@@ -30,11 +23,11 @@ const CartItems = db.define('cartItems', {
       min: 0
     },
     default: 0
-  },
-  //is this the name/info of the product?
-  content: {
-    type: Sequelize.TEXT
   }
+  //is this the name/info of the product?
+  // content: {
+  //   type: Sequelize.TEXT
+  // }
 })
 
 module.exports = CartItems
