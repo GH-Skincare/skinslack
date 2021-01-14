5014e881ff9514b5a942fbe32724f1b332a7d8a9
Jess <jess.jonesr@gmail.com>
2021-01-14 17:35:39 -0500

    (feat): created all products view component

---
+++ client/components/allproducts.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class AllProducts extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>ALL PRODUCTS</h1>
+      </div>
+    )
+  }
+}
+
+export default AllProducts
