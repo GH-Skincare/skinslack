36f2c5a5ce2f67a50aaf91a2c357d01108c3f416
Katerina Lina Papaloukas <katpapaloukas@gmail.com>
2021-01-17 15:08:36 -0500

    (css): nav bar from bootstrap-links working

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -3,37 +3,79 @@ import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
+import {Navbar, Nav, NavDropdown} from 'react-bootstrap'
 
-const Navbar = ({handleClick, isLoggedIn}) => (
+const Navigationbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1 className="title">JIL SkinCare</h1>
-    <h1 className="title-headline">Skincare That Lasts...</h1>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home" className="nav-links">
-            Home
-          </Link>
-          <a href="#" onClick={handleClick} className="nav-links">
-            Logout
-          </a>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login" className="nav-links">
-            Login
-          </Link>
-          <Link to="/signup" className="nav-links">
-            Sign Up
-          </Link>
-        </div>
-      )}
-    </nav>
-    <hr />
+    {/* <h1 className="title">JIL SkinCare</h1>
+    <h1 className="title-headline">Skincare That Lasts...</h1> */}
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Navbar.Brand href="#home">JIL SkinCare</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="#AllProducts">All Products</Nav.Link>
+        </Nav>
+        <Nav>
+          <nav>
+            {isLoggedIn ? (
+              <div>
+                {/* The navbar will show these links after you log in */}
+                <Link to="/home" className="nav-links">
+                  Home
+                </Link>
+                <a href="#" onClick={handleClick} className="nav-links">
+                  Logout
+                </a>
+              </div>
+            ) : (
+              <div>
+                {/* The navbar will show these links before you log in */}
+                <Link to="/login" className="nav-links">
+                  Login
+                </Link>
+                <Link to="/signup" className="nav-links">
+                  Sign Up
+                </Link>
+              </div>
+            )}
+          </nav>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   </div>
 )
+
+// const Navbar = ({handleClick, isLoggedIn}) => (
+//   <div>
+//     <h1 className="title">JIL SkinCare</h1>
+//     <h1 className="title-headline">Skincare That Lasts...</h1>
+//     <nav>
+//       {isLoggedIn ? (
+//         <div>
+//           {/* The navbar will show these links after you log in */}
+//           <Link to="/home" className="nav-links">
+//             Home
+//           </Link>
+//           <a href="#" onClick={handleClick} className="nav-links">
+//             Logout
+//           </a>
+//         </div>
+//       ) : (
+//         <div>
+//           {/* The navbar will show these links before you log in */}
+//           <Link to="/login" className="nav-links">
+//             Login
+//           </Link>
+//           <Link to="/signup" className="nav-links">
+//             Sign Up
+//           </Link>
+//         </div>
+//       )}
+//     </nav>
+//     <hr />
+//   </div>
+// )
 
 /**
  * CONTAINER
@@ -52,7 +94,7 @@ const mapDispatch = dispatch => {
   }
 }
 
-export default connect(mapState, mapDispatch)(Navbar)
+export default connect(mapState, mapDispatch)(Navigationbar)
 
 /**
  * PROP TYPES
