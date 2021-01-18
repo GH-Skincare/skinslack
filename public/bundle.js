a6ecac93cb537e58a2df6826c3aab8870ef450bb
Katerina Lina Papaloukas <katpapaloukas@gmail.com>
2021-01-18 12:36:13 -0500

    (feat): add route for admin view

--- public/bundle.js
+++ public/bundle.js
@@ -220,6 +220,111 @@ var mapDispatch = function mapDispatch(dispatch) {
 
 /***/ }),
 
+/***/ "./client/components/adminView.js":
+/*!****************************************!*\
+  !*** ./client/components/adminView.js ***!
+  \****************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _store_products__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../store/products */ "./client/store/products.js");
+/* harmony import */ var react_redux__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! react-redux */ "./node_modules/react-redux/es/index.js");
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+//admin view should be able to see all products component
+//the all products component should also have buttons to do the following:
+// 1. add to the inventory
+// 2. remove from the inventory
+// 3. delete entire product
+// 4. have a form to create a new product
+// logic: if user is admin: SHOW THIS PAGE, else show all products view
+
+
+ // import {Button} from 'react-bootstrap'
+
+var adminView =
+/*#__PURE__*/
+function (_React$Component) {
+  _inherits(adminView, _React$Component);
+
+  function adminView() {
+    _classCallCheck(this, adminView);
+
+    return _possibleConstructorReturn(this, _getPrototypeOf(adminView).apply(this, arguments));
+  }
+
+  _createClass(adminView, [{
+    key: "componentDidMount",
+    value: function componentDidMount() {
+      this.props.loadProducts();
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
+        className: "admin-view-headline"
+      }, "Admin View"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+        className: "admin-view-container"
+      }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("ul", null, this.props.products.map(function (product) {
+        return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+          className: "all-products",
+          key: product.id
+        }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", {
+          className: "product-stats"
+        }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+          className: "product-name"
+        }, product.name), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+          className: "product-image"
+        }, product.imageUrl), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+          className: "product-summary"
+        }, product.summary), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+          className: "product-price"
+        }, product.price), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("br", null), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", null, "\u2B50\uFE0F \u2B50 \u2B50\uFE0F \u2B50\uFE0F \u2B50\uFE0F "))));
+      }))));
+    }
+  }]);
+
+  return adminView;
+}(react__WEBPACK_IMPORTED_MODULE_0___default.a.Component);
+
+var mapState = function mapState(state) {
+  return {
+    products: state.products
+  };
+};
+
+var mapDispatch = function mapDispatch(dispatch) {
+  return {
+    loadProducts: function loadProducts() {
+      return dispatch(Object(_store_products__WEBPACK_IMPORTED_MODULE_1__["fetchProducts"])());
+    }
+  };
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Object(react_redux__WEBPACK_IMPORTED_MODULE_2__["connect"])(mapState, mapDispatch)(adminView)); // export default (adminView)
+
+/***/ }),
+
 /***/ "./client/components/allproducts.js":
 /*!******************************************!*\
   !*** ./client/components/allproducts.js ***!
@@ -749,7 +854,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _store__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./store */ "./client/store/index.js");
 /* harmony import */ var _components_allproducts__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./components/allproducts */ "./client/components/allproducts.js");
 /* harmony import */ var _components_singleProduct__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ./components/singleProduct */ "./client/components/singleProduct.js");
-/* harmony import */ var _components_activeOrder__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! ./components/activeOrder */ "./client/components/activeOrder.js");
+/* harmony import */ var _components_activeOrder__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ./components/activeOrder */ "./client/components/activeOrder.js");
+/* harmony import */ var _components_adminView__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! ./components/adminView */ "./client/components/adminView.js");
 function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -767,6 +873,7 @@ function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.g
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
 
 function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
 
 
 
@@ -821,7 +928,11 @@ function (_Component) {
       }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_2__["Route"], {
         exact: true,
         path: "/cart",
-        component: _components_activeOrder__WEBPACK_IMPORTED_MODULE_9__["default"]
+        component: _components_activeOrder__WEBPACK_IMPORTED_MODULE_8__["default"]
+      }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_2__["Route"], {
+        exact: true,
+        path: "/admin",
+        component: _components_adminView__WEBPACK_IMPORTED_MODULE_9__["default"]
       })), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(react_router_dom__WEBPACK_IMPORTED_MODULE_2__["Route"], {
         component: _components__WEBPACK_IMPORTED_MODULE_4__["Login"]
       }));
